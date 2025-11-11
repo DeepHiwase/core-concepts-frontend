967d39090fe6d8dbcf313d75058099004318e452
Deep <diphiwase0987@gmail.com>
2025-11-11 14:47:48 +0530

    font-style, italic

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,9 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <span className="underline decoration-wavy decoration-1 sm:overline sm:decoration-dotted md:no-underline lg:line-through lg:decoration-dashed lg:decoration-5 underline-offset-3 hover:underline-offset-4">
-        Hi Grid & Flex!
-      </span>
+      <span className="italic md:not-italic">Hi Grid & Flex!</span>
       <div>
-        <a
-          href="https://x.com"
-          className="text-blue-600 font-bold no-underline hover:underline hover:-underline-offset-10 md:hover:decoration-[#99bb82]/95 decoration-red-600/45 lg:overline lg:decoration-3 lg:decoration-regal-blue lg:decoration-double"
-        >
+        <a href="https://x.com" className="custom_oblique">
           Deep
         </a>
       </div>
