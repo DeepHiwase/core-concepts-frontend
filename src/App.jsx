bf84a90dc8e460bf8ce46c7ae1d3579f238002fb
Deep <diphiwase0987@gmail.com>
2025-11-11 11:46:01 +0530

    grid-minmax, auto-fit, auto-fill, min-content, max-content, flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import "./App.css";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <span>Hi Grid & Flex!</span>
+      <div className="container">
+        <div>1sabdbjiennnxjjiw</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1sfnsdnfna</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+      </div>
+    </>
+  );
 };
 
 export default App;
