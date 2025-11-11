88fd84002b0968d36ddc1618efa4f85c0795ef25
Deep <diphiwase0987@gmail.com>
2025-11-11 15:23:58 +0530

    list-style-image-position

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,45 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <span className="uppercase sm:normal-case md:lowercase lg:capitalize">Hi Grid & Flex!</span>
+      <span className="">Hi Grid & Flex!</span>
       <div>
         <a href="https://x.com" className="">
           Deep
         </a>
       </div>
+      <ul className="list-disc md:list-none pl-5">
+        <li>Cake</li>
+        <li>Pastery</li>
+        <li>Chocolate</li>
+        <li>Lemon</li>
+      </ul>
+      <hr />
+      <ol className="list-decimal md:list-none pl-5">
+        <li>Cake</li>
+        <li>
+          <ol className="list-decimal sm:list-[circle] md:list-[upper-roman] lg:list-[lower-roman] pl-4">
+            <li>Hi nested list</li>
+            <li>Hi nested list 2</li>
+            <li>Hi nested list 3</li>
+          </ol>
+        </li>
+        <li>Chocolate</li>
+        <li>Lemon</li>
+      </ol>
+      <hr />
+      <ul className="list-disc list-outside md:list-inside pl-5">
+        <li>Cakealsndalsnnalllllllllllllllllllllllasndaskmdamsdamlnasdl</li>
+        <li>Pastery</li>
+        <li>Chocolate</li>
+        <li>Lemon</li>
+      </ul>
+      <hr />
+      <ul className="list-disc list-image-none sm:list-image-[url(./checkmark.png)] md:list-image-[url(./tailwind-checkmark.svg)] pl-5">
+        <li>Cakealsndalsnnalllllllllllllllllllllllasndaskmdamsdamlnasdl</li>
+        <li>Pastery</li>
+        <li>Chocolate</li>
+        <li>Lemon</li>
+      </ul>
       {/* <div className="container">
         <div>1sabdbjiennnxjjiw</div>
         <div>1</div>
